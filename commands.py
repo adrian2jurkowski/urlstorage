c9a50a34dbb305536b9cc6bf07b85477c03f4a9c
Adrian <a.jurkowski@interia.eu>
2023-02-21 11:03:43 +0100

    Api
    nauka z sqlite, tworzenie api, bibliteki click, getenv, flask

---
+++ commands.py
@@ -0,0 +1,43 @@
+import click
+from database import Database
+from os import getenv
+from repositories.urls import save, fetch_categories, fetch_urls
+
+
+@click.group()
+def cli():
+    pass
+
+
+@click.command(name='setup')
+def setup_command():
+    print('Tworze tabele w bazie danych')
+    db = Database(getenv('DB_NAME'))
+    db.create_table('CREATE TABLE urls (id INTEGER PRIMARY KEY AUTOINCREMENT, category TEXT, url TEXT)')
+
+
+@click.command(name='add')
+@click.argument('category')
+@click.argument('url')
+def add_command(category: str, url: str):
+    print('Dodaje nowy adres url')
+    save(category, url)
+
+
+@click.command(name='categories')
+def list_command():
+    print('Lista kategorii:')
+
+
+    for name in fetch_categories():
+        print(name[0])
+
+
+@click.command(name='index')
+@click.argument('category')
+def index_command(category: str):
+    print(f'Lista linków z kategorii {category}:')
+
+
+    for link in fetch_urls(category):
+        print(link[2])
